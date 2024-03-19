fc06bdd334f9f9fbd05f26195d829d7758cb91da
Loveberry <lachlanloveberry@gmail.com>
2024-03-19 18:58:56 +1000

    add message boxes

--- src/MessageHisoryPopup.tsx
+++ src/MessageHisoryPopup.tsx
@@ -1,4 +1,12 @@
-import { Center, Loader, Modal, ModalProps } from "@mantine/core";
+import {
+  Box,
+  Center,
+  Flex,
+  Loader,
+  Modal,
+  ModalProps,
+  Text,
+} from "@mantine/core";
 import { useMessageHistory } from "./hooks/useMessageHistory";
 
 interface MessageHistoryPopup extends ModalProps {
@@ -11,12 +19,33 @@ export function MessageHistoryPopup({
   mobile,
   ...rest
 }: MessageHistoryPopup) {
-  const { data, isLoading, status, error } = useMessageHistory(mobile);
-  console.log(data);
+  const { data, isLoading } = useMessageHistory(mobile);
 
   return (
     <Modal title={`Message history with ${name}`} {...rest}>
-      <Center>{isLoading && <Loader />}</Center>
+      <Center h={96}>{isLoading && <Loader />}</Center>
+      {data && (
+        <Flex direction="column" gap="md">
+          {data.data.map((message) => (
+            <Box
+              style={{
+                backgroundColor:
+                  message.direction === "outgoing" ? "lightgray" : "lightblue",
+                overflow: "hidden",
+                padding: 12,
+                borderRadius: 12,
+                alignSelf:
+                  message.direction === "outgoing" ? "flex-start" : "flex-end",
+              }}
+            >
+              <Text>{message.sms_info.body}</Text>
+              <Text size="xs" c="gray">
+                {message.sms_date} {message.sms_time}
+              </Text>
+            </Box>
+          ))}
+        </Flex>
+      )}
     </Modal>
   );
 }
